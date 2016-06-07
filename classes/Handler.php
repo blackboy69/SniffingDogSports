b275405028d98aa45f1dd3a6f3fe16444f2d828c
harley <harley@yourshowcase.net>
2016-06-07 09:37:21 -0500

    updates

--- classes/Handler.php
+++ classes/Handler.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Sniffing Dog Sports - Global Include
+ * Sniffing Dog Sports - Handler class
  * June 2016 by Harley H. Puthuff
  * Copyright 2016, Sniffing Dog Sports, Ltd.
  */
 
-class SDS {
+class Handler extends Container {
 	
 }
 ?>
