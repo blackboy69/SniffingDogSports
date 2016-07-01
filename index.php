be8c034c99d2546fb3df2155d3a6594a331f6ea6
harley <harley@harleyws>
2016-07-01 09:41:24 -0500

    updates

--- index.php
+++ index.php
@@ -34,14 +34,22 @@ include_once("classes/SDS.php");
 		</div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		<ul class="nav navbar-nav">
-			<li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
-			<li><a href="#">Our Mission</a></li>
-			<li><a href="#">Our Services</a></li>
-			<li><a href="#">Register</a></li>
-			<li><a href="#">Contact Us</a></li>
+			<li class="active"><a href="#" title="Return to the Home page"
+				><span class="glyphicon glyphicon-home"></span> Home<span
+				  class="sr-only">(current)</span></a></li>
+			<li><a href="#" title="Who we are and why we do this"><span
+					class="glyphicon glyphicon-globe"></span> Our Mission</a></li>
+			<li><a href="#" title="Events we sponsor and score"><span
+					class="glyphicon glyphicon-time"></span> Our Trials</a></li>
+			<li><a href="#" title="Register with Sniffing Dog Sports"><span
+					class="glyphicon glyphicon-edit"></span> Register</a></li>
+			<li><a href="#" title="Contact us via Email, Phone and Mail"><span
+					class="glyphicon glyphicon-envelope"></span> Contact Us</a></li>
 			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-				   accesskey="" aria-expanded="false">Pages <span class="caret"></span></a>
+				   title="Map of our site resources"
+				   accesskey="" aria-expanded="false"><span
+					class="glyphicon glyphicon-list"></span> Site Map <span class="caret"></span></a>
 				<ul class="dropdown-menu" role="menu">
 					<li><a href="#">Home page</a></li>
 					<li><a href="#">Our Mission</a></li>
@@ -58,11 +66,13 @@ include_once("classes/SDS.php");
 			<div class="form-group">
 				<input type="text" class="form-control" placeholder="Search for ...">
 			</div>
-			<button type="submit" class="btn btn-default">Search</button>
+			<button type="submit" class="btn btn-default"><span
+				class="glyphicon glyphicon-search"></span> Search</button>
 		</form>
 		<ul class="nav navbar-nav navbar-right">
-			<li><button class="btn btn-success" style="margin-top:8px">Login</button></li>
-			<!--li><a href="#" class="btn btn-sm btn-default">Login</a></li-->
+			<li><button class="btn btn-success" style="margin-top:8px"
+					title="Click to Login"><span
+					class="glyphicon glyphicon-lock"></span> Login</button></li>
 		</ul>
     </div>
 	</div>
